9e00a39d11dc0ea99576ec921b204c4aeb2b3b86
PaulHaze <dev@artemist.com.au>
2023-08-23 15:41:16 +1000

    strip down main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-[100vh] bg-gradient-to-br from-green-300/50 to-sky-800/50 flex items-center justify-center px-4">
-      <div className="px-4 mt-10 h-100">
-        <h1 className="font-bold text-center text-transparent bg-clip-text bg-gradient-to-b from-indigo-400 to-violet-800">
-          Im your new Tailwind Project!
-        </h1>
-        <p className="text-[#504e63] text-center">
-          Delete this and make something great!
-        </p>
-      </div>
+    <div className="flex-center h-screen">
+      <h4>Barebones TW Next Starter</h4>
     </div>
   );
 }
