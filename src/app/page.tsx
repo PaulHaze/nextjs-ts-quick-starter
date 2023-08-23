8983ab74fad89d799c67455ef2415663178ee46a
PaulHaze <dev@artemist.com.au>
2023-08-23 15:31:01 +1000

    update all the configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-[100vh] bg-gradient-to-br from-green-300/50 to-sky-800/50 flex items-center justify-center px-5">
-      <div className="flex flex-col justify-center bg-white rounded-lg shadow-2xl p-md sm:p-lg max-w-md">
+    <div className="h-[100vh] bg-gradient-to-br from-green-300/50 to-sky-800/50 flex items-center justify-center px-4">
+      <div className="px-4 mt-10 h-100">
         <h1 className="font-bold text-center text-transparent bg-clip-text bg-gradient-to-b from-indigo-400 to-violet-800">
           Im your new Tailwind Project!
         </h1>
