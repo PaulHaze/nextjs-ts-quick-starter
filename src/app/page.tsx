3c20377ccd345f963636658b6f256a58b34a16e7
PaulHaze <dev@artemist.com.au>
2024-02-21 11:35:35 +1100

    bg change in hompage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex-center h-screen bg-gray-300">
+    <div className="flex-center h-screen bg-gray-100">
       <h4>Barebones TW Next Starter</h4>
     </div>
   );
