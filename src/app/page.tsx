f9fb4d7cf95000403215ee7164122d80c2769908
PaulHaze <dev@artemist.com.au>
2024-05-06 13:03:22 +1000

    dev deps bumped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex-center h-screen bg-gray-100">
-      <h4>Barebones TW Next Starter</h4>
+      <h4 className="">Barebones TW Next Starter</h4>
     </div>
   );
 }
