98e64839318d04cb9a48d10898db30b9cf983758
PaulHaze <dev@artemist.com.au>
2024-06-26 12:12:33 +1000

    add zustand reference to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="h-screen flex-col bg-gray-50 flex-center">
       <h4>Barebones TW Next Starter</h4>
+      <h5>With Zustand</h5>
       <Counter />
     </div>
   );
