1fd4f17aeddafa815b2dea730a863cbead674692
PaulHaze <dev@artemist.com.au>
2024-06-26 12:16:25 +1000

    add about page to its own folder

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function About() {
   return (
-    <div className="h-screen flex-col bg-gray-50 flex-center">
+    <div className="flex-cente h-screen flex-col bg-gray-50">
       <h4>About Page</h4>
       <Link href="/">Home</Link>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-50 flex-center">
+    <div className="h-screen flex-col bg-gray-50 flex-center">
       <h4>Barebones TW Next Starter</h4>
+      <Link href="/about">About</Link>
     </div>
   );
 }
