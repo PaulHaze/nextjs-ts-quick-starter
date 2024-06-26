7edeb71c88137f780590ff926ee7df2780656917
PaulHaze <dev@artemist.com.au>
2024-06-26 12:14:29 +1000

    style classes rearranged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ThemeToggle } from '@/components/ui';
 
 export default function Home() {
   return (
-    <div className="bg-base-100 relative h-screen">
+    <div className="relative h-screen bg-base-100">
       <div className="absolute right-2 top-3">
         <ThemeToggle />
       </div>
