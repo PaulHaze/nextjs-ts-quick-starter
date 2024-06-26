79b5d27f1ea870cf54987eb2f8a4097cbec088f0
PaulHaze <dev@artemist.com.au>
2024-06-26 20:36:29 +1000

    add and run basic playwright test

--- e2e/basic_example.spec.ts
+++ e2e/basic_example.spec.ts
@@ -6,7 +6,7 @@ test('should navigate to the about page', async ({ page }) => {
   // Find an element with the text 'About' and click on it
   await page.click('text=About');
   // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL('http://localhost:3000/about');
+  await expect(page).toHaveURL('/about');
   // The new page should contain an h1 with "About"
   await expect(page.locator('h1')).toContainText('About');
 });
