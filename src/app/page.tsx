33803a510b5751ab394248654c3273f466147e30
PaulHaze <dev@artemist.com.au>
2024-06-26 11:28:35 +1000

    add basic zustand counter to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Counter } from '@/components';
+
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-50 flex-center">
+    <div className="h-screen flex-col bg-gray-50 flex-center">
       <h4>Barebones TW Next Starter</h4>
+      <Counter />
     </div>
   );
 }
