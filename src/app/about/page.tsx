1fcc2f987caabc171a1c7661f2dc4c5554755a1d
PaulHaze <dev@artemist.com.au>
2024-06-26 12:17:10 +1000

    style fixes to home + about pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function About() {
   return (
-    <div className="flex-cente h-screen flex-col bg-gray-50">
+    <div className="h-screen flex-col bg-gray-50 flex-center">
       <h4>About Page</h4>
       <Link href="/">Home</Link>
     </div>
