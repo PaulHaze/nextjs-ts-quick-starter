4d7cb318613845037f860831d9de05f6ac1ac3c5
PaulHaze <dev@artemist.com.au>
2024-06-26 11:31:40 +1000

    add extra parenthesis to counterStore

--- src/stores/useCounterStore.ts
+++ src/stores/useCounterStore.ts
@@ -6,7 +6,7 @@ type CounterState = {
   resetCounter: () => void;
 };
 
-export const useCounterStore = create<CounterState>((set) => ({
+export const useCounterStore = create<CounterState>()((set) => ({
   counter: 0,
   increaseCounter: () => set((state) => ({ counter: state.counter + 1 })),
   resetCounter: () => set({ counter: 0 }),
