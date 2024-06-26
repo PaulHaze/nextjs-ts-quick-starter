82bf0c5993ae427b4ed057a8e3a0244723b0591b
PaulHaze <dev@artemist.com.au>
2024-06-26 12:13:42 +1000

    tweaks to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import { ThemeToggle } from '@/components/ui';
 
 export default function Home() {
   return (
-    <div className="relative h-screen bg-base-100">
+    <div className="bg-base-100 relative h-screen">
       <div className="absolute right-2 top-3">
         <ThemeToggle />
       </div>
       <div className="flex justify-center">
-        <h4 className="mt-10">Barebones TW Next Starter w/ DaisyUI</h4>
+        <h4 className="mt-10">Barebones TW Next Starter</h4>
+        <h5>With Daisy UI</h5>
       </div>
     </div>
   );
