b608d956047ff0a486aeaabb07835868692aefb0
PaulHaze <dev@artemist.com.au>
2024-06-27 20:32:03 +1000

    home page clean up adjust basic tests

--- e2e/basic_example.spec.ts
+++ e2e/basic_example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('should show the home page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/');
-  await expect(page.locator('h1')).toContainText('Home');
+  await expect(page.locator('h1')).toContainText('Barebones TW Next Starter');
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import { ThemeToggle } from '@/components/ui';
 
 export default function Home() {
   return (
-    <div className="relative h-screen">
+    <div className="relative">
       <div className="absolute right-2 top-3">
         <ThemeToggle />
       </div>
       <div className="flex flex-col items-center pt-10">
-        <h1>Home</h1>
-        <h4 className="">Barebones TW Next Starter</h4>
-        <h5>With Daisy UI & Zustand</h5>
+        <h1>Barebones TW Next Starter</h1>
       </div>
     </div>
   );
