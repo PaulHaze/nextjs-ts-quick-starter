812686c4913c3f967b9badc4692c5200db157b3b
PaulHaze <dev@artemist.com.au>
2024-07-01 13:06:00 +1000

    update TW config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,10 @@
 import type { Config } from 'tailwindcss';
-import typographyPlugin from '@tailwindcss/typography';
-import daisyui from 'daisyui';
-
-import themes from 'daisyui/src/theming/themes';
 
 import plugin from 'tailwindcss/plugin';
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: ['selector', '[data-theme="dark"]'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
   theme: {
     fontSize: {
       xs: '0.75rem',
@@ -46,9 +42,6 @@ export default {
       center: true,
     },
     extend: {
-      colors: {
-        'primary-muted': 'oklch(var(--primary-muted-oklch) / <alpha-value>)',
-      },
       minWidth: {
         xs: '431px',
         sm: '640px',
@@ -81,8 +74,6 @@ export default {
     },
   },
   plugins: [
-    typographyPlugin,
-    daisyui,
     plugin(({ addUtilities, matchUtilities, theme }) => {
       addUtilities({
         '.flex-center': {
@@ -144,75 +135,4 @@ export default {
       );
     }),
   ],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          ...themes.light,
-          '--primary-muted': '338 83% 66%', // this is here just as an example of HSL
-          '--primary-muted-oklch': '62.49% 0.1667 272.93', // this is here just as an example of OKLCH
-          neutral: '#A7ADBA',
-          'neutral-content': '#2d343f',
-          // add individual colours here to customize theme
-          /* 
-					*EXAMPLE - ESSENTIAL
-					"primary": "#a991f7",
-          "secondary": "#f6d860",
-          "accent": "#37cdbe",
-          "neutral": "#3d4451",
-          "base-100": "#ffffff",
-
-					*EXAMPLE - OPTIONAL
-					* These are set automatically based on the colours chosen above
-					* But they can be overriden
-					"primary-content": "#d0e3ff", - Colour of text in 'Primary'
-					"secondary-content": "#0a0900", - Colour of text in 'Secondary'
-					"accent-content": "#00050d",- Colour of text in 'Accent'
-					"neutral-content": "#d2cdd2", - Colour of text in 'Neutral
-					"base-200": "#dbdbdb", - Slightly darker shade of base-100
-					"base-300": "#bbbbbb", - Even darker shade of base-100
-					"base-content": "#161616", - Contrasting colour for text on base-100
-
-					* Additional colours to be added if needed
-					"info": "#00b1ff",
-					"info-content": "#000c16",
-					"success": "#85ee00",
-					"success-content": "#061400",
-					"warning": "#c28700",
-					"warning-content": "#0e0600",
-					"error": "#ff768e",
-					"error-content": "#160507",
-
-
-					* Additional CSS variables used
-          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
-          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
-          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
-          "--animation-btn": "0.25s", // duration of animation when you click on button
-          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
-          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
-          "--border-btn": "1px", // border width of buttons
-          "--tab-border": "1px", // border width of tabs
-          "--tab-radius": "0.5rem", // border radius of tabs */
-        },
-      },
-      {
-        dark: {
-          ...themes.dark,
-          '--primary-muted': '38 83% 66%',
-          '--primary-muted-oklch': '48.11% 0.1667 272.93',
-          neutral: '#2d343f',
-          'neutral-content': '#A7ADBA',
-          // add individual colours here to customize theme
-        },
-      },
-    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"],
-    darkTheme: 'dark', // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ':root', // The element that receives theme color CSS variables
-  },
 } satisfies Config;
