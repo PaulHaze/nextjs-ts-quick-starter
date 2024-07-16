2b53d0089cbd0703bd0924dc15d4d1aa822a97de
PaulHaze <dev@artemist.com.au>
2024-07-16 12:14:16 +1000

    fix content path in tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss';
 import plugin from 'tailwindcss/plugin';
 
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,json}'],
   darkMode: 'selector',
   theme: {
     fontSize: {
