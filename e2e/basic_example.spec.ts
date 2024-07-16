6b4da0793a882c4fd328d37c19d28961192a6dfc
PaulHaze <dev@artemist.com.au>
2024-07-16 15:49:46 +1000

    remove uneeded files

--- e2e/basic_example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('should show the home page', async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('/');
-  await expect(page.locator('h1')).toContainText('Barebones TW Next Starter');
-});
