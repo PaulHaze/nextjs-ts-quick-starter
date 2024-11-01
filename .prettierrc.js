f3f42cdc432329d653e0ef06dc4330bb6443e30f
PaulHaze <dev@artemist.com.au>
2024-11-01 16:38:58 +1100

    update all deps

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 80,
-  tabWidth: 2,
-};
---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 80,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+
+};
